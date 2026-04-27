37ebc1b583ff1d41cde075263361a5dcf8f56539
Miroslav Lincovschi <lincovschi@proton.me>
2026-04-27 11:43:22 +0300

    feat(home): add currently section

---
+++ src/components/Currently.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="mt-8 hidden animate-fadeIn lg:block">
+    <h3 class="mb-3 text-sm uppercase tracking-widest text-primary-variant duration-300">
+      currently
+    </h3>
+    <ul class="space-y-2 text-secondary duration-300">
+      <li class="flex gap-2">
+        <span class="text-primary">*</span>
+        <span>maintaining obscure.nvim</span>
+      </li>
+      <li class="flex gap-2">
+        <span class="text-primary">*</span>
+        <span>learning Swift and native app development</span>
+      </li>
+    </ul>
+  </section>
+</template>
